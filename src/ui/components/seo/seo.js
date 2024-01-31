32f61e55bc001e6827f7f7253ca1406caf5d42d1
mouhamed lamotte <lamottelymouhamed@gmail.com>
2024-01-31 01:27:42 +0000

    add web manifest and favicon

--- src/ui/components/seo/seo.js
+++ src/ui/components/seo/seo.js
@@ -1,6 +1,12 @@
-
-export const Seo = ({title, description}) =>{
-        return(
-            <div></div>
-        )
-}
+export const Seo = ({ title, description }) => {
+  return (
+    <>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      <link rel="manifest" href="/site.webmanifest" />
+    </>
+  );
+};
