3e5e84c9611d1f0b3560864d7a2e6b2714294cca
mouhamed lamotte <lamottelymouhamed@gmail.com>
2024-02-01 01:20:21 +0000

    add somme p

--- src/pages/index.js
+++ src/pages/index.js
@@ -91,7 +91,7 @@ export default function Home() {
           </section>
 
           {/* Buy Me Coffee Section */}
-          <section className="bg-lime-200 py-16 mb-8">
+          <section className="bg-lime-200 py-16 mb-8 px-3">
             <div className="container mx-auto text-center">
               <h2 className="text-3xl font-bold mb-8 ">
                 Buy Me a Coffee ☕
